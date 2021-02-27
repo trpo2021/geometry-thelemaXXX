93cfa09695a754c086c5b9e15a4469e56ecdee2f
unknown <you@example.com>
2021-02-27 15:15:47 +0700

    Coordinate values ​​can now be negative, error correction in conditions, code simplification

--- main.cpp
+++ main.cpp
@@ -10,73 +10,68 @@ int main()
     string cir;
     cin.getline(circle, 80);
     cir.assign(circle);
-    int left_par = cir.find('(');
-    int right_par = cir.find(')', left_par);
+    int left_bracket = cir.find('(');
+    int right_bracket = cir.find(')');
     int first_space = cir.find(' ');
-    bool IsDataTrue = true;
-    if (left_par == -1 || right_par == -1) {
+    bool Condition_is_met = true;
+    if (left_bracket == -1 || right_bracket == -1) {
         cout << "Error at column 0: parenthesis expected" << endl;
-        IsDataTrue = false;
+        Condition_is_met = false;
     }
-    int CircleTrue = cir.compare(0, first_space, "circle") * cir.compare(0, left_par, "circle")
-                     * cir.compare(0, first_space, "CIRCLE") * cir.compare(0, left_par, "CIRCLE");
+    int CircleTrue = cir.compare(0, first_space, "circle") * cir.compare(0, left_bracket, "circle")
+        		   * cir.compare(0, first_space, "CIRCLE") * cir.compare(0, left_bracket, "CIRCLE");
     if (CircleTrue != 0) {
         cout << "Error at column 1: circle expected" << endl;
-        IsDataTrue = false;
+        Condition_is_met = false;
     }
-    int comma = cir.find(',', left_par);
+    int comma = cir.find(',');
     if (comma == -1) {
         cout << "Error at column 2: wrong data" << endl;
-        IsDataTrue = false;
+        Condition_is_met = false;
     }
-    bool IsNumber = false;
-    int lcount = 0;
-    int rcount = 0;
-    for (int i = left_par + 1; i <= comma; i++) {
-        if ((cir[i] == ' ' || cir[i] == ',') && IsNumber) {
-            lcount++;
-            IsNumber = false;
+    bool is_a_number = false;
+    int left_part = 0;
+    int right_part = 0;
+    for (int i = left_bracket + 1; i <= comma; i++) {
+        if (cir[i] == ' ' || cir[i] == ',') {
+            left_part++;
+            is_a_number = false;
         }
-        if ((cir[i] >= '0' && cir[i] <= '9') || cir[i] == ',') {
-            IsNumber = true;
-        } else if (cir[i] == ' ')
-            continue;
+        if ((cir[i] >= '0' && cir[i] <= '9') || cir[i] == '-' || cir[i] == ',') is_a_number = true; 
+		else if (cir[i] == ' ') continue;
         else {
             cout << "Error at column 3: wrong symbols" << endl;
-            IsDataTrue = false;
+            Condition_is_met = false;
             break;
         }
     }
-    IsNumber = false;
-    for (int i = comma + 1; i <= right_par; i++) {
-        if ((cir[i] == ' ' || cir[i] == ')') && IsNumber) {
-            rcount++;
-            IsNumber = false;
+    is_a_number = false;
+    for (int i = comma + 1; i <= right_bracket; i++) {
+        if (cir[i] == ' ' || cir[i] == ')') {
+            right_part++;
+            is_a_number = false;
         }
-        if ((cir[i] >= '0' && cir[i] <= '9' || cir[i] == '.') || cir[i] == ')')
-            IsNumber = true;
-        else if (cir[i] == ' ')
-            continue;
+        if ((cir[i] >= '0' && cir[i] <= '9') || cir[i] == '.' || cir[i] == ')') is_a_number = true;
+        else if (cir[i] == ' ') continue;
         else {
-            cout << "Error at column 3: wrong symbols" << endl;
-            IsDataTrue = false;
+            cout << "Error at column 3: wrong symbols" << endl;	
+            Condition_is_met = false;
             break;
         }
     }
-    if (lcount != 2 || rcount != 1) {
+    if ((left_part == 1 || left_part == 0) || right_part == 0) {
         cout << "Error at column 4: wrong amount of data" << endl;
-        IsDataTrue = false;
+        Condition_is_met = false;
     }
-    for (int i = right_par + 2; i < 80; i++) {
+    for (int i = right_bracket + 2; i < 80; i++) {
         if (circle[i] != ' ') {
             cout << "Error at column 5: unexpected token" << endl;
-            IsDataTrue = false;
+            Condition_is_met = false;
             break;
         }
     }
-    if (IsDataTrue) {
+    if (Condition_is_met) {
         cout << cir;
     }
-    return 0;
 }
 
