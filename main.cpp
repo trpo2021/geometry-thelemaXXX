aef5a533d08f08cd43efaccaf62da343632d65cc
unknown <you@example.com>
2021-02-28 00:25:05 +0700

    Fix 6 and 7 output, using clang-format

--- main.cpp
+++ main.cpp
@@ -23,38 +23,35 @@ int main()
                      * txt.compare(0, left_bracket, "CIRCLE");
     int c = txt.find('c');
     int e = txt.find('e');
-    while(1) {
-    if (c - 1 == first_space) {
-        cout << "Error at column 1: space before circle" << endl;
-        Condition_is_met = false;
-        break;
+    while (1) {
+        if (c - 1 == first_space) {
+            cout << "Error at column 1: space before circle" << endl;
+            Condition_is_met = false;
+            break;
+        } else if (e + 1 == first_space) {
+            cout << "Error at column 2: space after circle" << endl;
+            Condition_is_met = false;
+            break;
+        } else if (CircleTrue != 0) {
+            cout << "Error at column 3: circle not entered" << endl;
+            Condition_is_met = false;
+            break;
+        } else
+            break;
     }
-    else if (e + 1 == first_space) {
-        cout << "Error at column 2: space after circle" << endl;
-        Condition_is_met = false;
-        break;
-    }
-    else if (CircleTrue != 0) {
-        cout << "Error at column 3: circle not entered" << endl;
-        Condition_is_met = false;
-        break;
-    }
-    else break;
-	}
     int comma = txt.find(',');
     if (comma == -1) {
-        cout << "Error at column 4: wrong data" << endl;
+        cout << "Error at column 4: comma not entered" << endl;
         Condition_is_met = false;
     }
     for (int i = left_bracket + 1; i <= comma; i++) {
-        if ((txt[i] >= '0' && txt[i] <= '9') || txt[i] == '-' || txt[i] == ' ')
+        if ((txt[i] >= '0' && txt[i] <= '9') || txt[i] == '-' || txt[i] == ' ' || txt[i] == ',')
             continue;
         else if (txt[i] == '.') {
-        	cout << "Error at column 5: coordinates must be integers" << endl;
-        	Condition_is_met = false;
-        	break;
-        }
-        else {
+            cout << "Error at column 5: coordinates must be integers" << endl;
+            Condition_is_met = false;
+            break;
+        } else {
             cout << "Error at column 6: incorrect symbols before comma" << endl;
             Condition_is_met = false;
             break;
@@ -62,29 +59,31 @@ int main()
     }
     if (Condition_is_met != false) {
         for (int i = comma + 1; i <= right_bracket; i++) {
-            if ((txt[i] >= '0' && txt[i] <= '9') || txt[i] == '.'|| txt[i] == '-' || txt[i] == ',' || txt[i] == ' ' || txt[i] == ')')
+            if ((txt[i] >= '0' && txt[i] <= '9') || txt[i] == '.' || txt[i] == ',' || txt[i] == ' '
+                || txt[i] == ')')
                 continue;
             else if (txt[i] == '-') {
-            	cout << "Error at column 7: radius cannot be negative" << endl;
-        		Condition_is_met = false;
-        		break;
-			}
-            else {
+                cout << "Error at column 7: radius cannot be negative" << endl;
+                Condition_is_met = false;
+                break;
+            } else {
                 cout << "Error at column 8: incorrect symbols after comma" << endl;
                 Condition_is_met = false;
                 break;
             }
         }
     }
-	int j = 0;
-	for(int k = left_bracket; k <= right_bracket + 10; k++ ) {
-	 if (txt[k] == ',' || txt[k] == ')' || txt[k] == '(') j++;
-	 	if ((txt[k] >= '0' && txt[k] <= '9') || txt[k] == '-' || txt[k] == ',' || txt[k] == ' ') continue;
-    	else if (j > 3) {
-        cout << "Error at column 9: wrong amount of data" << endl;
-        Condition_is_met = false;
-        break;
-    }
+    int j = 0;
+    for (int k = left_bracket; k <= right_bracket + 10; k++) {
+        if (txt[k] == ',' || txt[k] == ')' || txt[k] == '(')
+            j++;
+        if ((txt[k] >= '0' && txt[k] <= '9') || txt[k] == '-' || txt[k] == ',' || txt[k] == ' ')
+            continue;
+        else if (j > 3) {
+            cout << "Error at column 9: wrong amount of data" << endl;
+            Condition_is_met = false;
+            break;
+        }
     }
     for (int i = right_bracket + 2; i < 80; i++) {
         if (text[i] != ' ') {
