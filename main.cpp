494a32b3026837fadef2d706cd0e26f15da498eb
unknown <you@example.com>
2021-02-27 11:44:20 +0700

    Minor fixes

--- main.cpp
+++ main.cpp
@@ -3,81 +3,79 @@
 using namespace std;
 int main()
 {
-    char input[80];
+    char circle[80];
     for (int i = 0; i < 80; i++) {
-        input[i] = ' ';
+        circle[i] = ' ';
     }
-    string inp;
-    cin.getline(input, 80);
-    inp.assign(input);
-    int left_par = inp.find('(');
-    int right_par = inp.find(')', left_par);
-    int first_space = inp.find(' ');
+    string cir;
+    cin.getline(circle, 80);
+    cir.assign(circle);
+    int left_par = cir.find('(');
+    int right_par = cir.find(')', left_par);
+    int first_space = cir.find(' ');
     bool IsDataTrue = true;
     if (left_par == -1 || right_par == -1) {
-        cout << "Error: parenthesis expected" << endl;
+        cout << "Error at column 0: parenthesis expected" << endl;
         IsDataTrue = false;
     }
-    int CircleTrue = inp.compare(0, first_space, "circle")
-            * inp.compare(0, left_par, "circle")
-            * inp.compare(0, first_space, "CIRCLE")
-            * inp.compare(0, left_par, "CIRCLE");
+    int CircleTrue = cir.compare(0, first_space, "circle") * cir.compare(0, left_par, "circle")
+                     * cir.compare(0, first_space, "CIRCLE") * cir.compare(0, left_par, "CIRCLE");
     if (CircleTrue != 0) {
-        cout << "Error: circle expected" << endl;
+        cout << "Error at column 1: circle expected" << endl;
         IsDataTrue = false;
     }
-    int comma = inp.find(',', left_par);
+    int comma = cir.find(',', left_par);
     if (comma == -1) {
-        cout << "Error: wrong data" << endl;
+        cout << "Error at column 2: wrong data" << endl;
         IsDataTrue = false;
     }
     bool IsNumber = false;
-    int lcount = 0; // must be 2
-    int rcount = 0; // must be 1
+    int lcount = 0;
+    int rcount = 0;
     for (int i = left_par + 1; i <= comma; i++) {
-        if ((inp[i] == ' ' || inp[i] == ',') && IsNumber) {
+        if ((cir[i] == ' ' || cir[i] == ',') && IsNumber) {
             lcount++;
             IsNumber = false;
         }
-        if ((inp[i] >= '0' && inp[i] <= '9') || inp[i] == ',') {
+        if ((cir[i] >= '0' && cir[i] <= '9') || cir[i] == ',') {
             IsNumber = true;
-        } else if (inp[i] == ' ')
+        } else if (cir[i] == ' ')
             continue;
         else {
-            cout << "Error: wrong symbols" << endl;
+            cout << "Error at column 3: wrong symbols" << endl;
             IsDataTrue = false;
             break;
         }
     }
     IsNumber = false;
     for (int i = comma + 1; i <= right_par; i++) {
-        if ((inp[i] == ' ' || inp[i] == ')') && IsNumber) {
+        if ((cir[i] == ' ' || cir[i] == ')') && IsNumber) {
             rcount++;
             IsNumber = false;
         }
-        if ((inp[i] >= '0' && inp[i] <= '9') || inp[i] == ')')
+        if ((cir[i] >= '0' && cir[i] <= '9') || cir[i] == ')')
             IsNumber = true;
-        else if (inp[i] == ' ')
+        else if (cir[i] == ' ')
             continue;
         else {
-            cout << "Error: wrong symbols" << endl;
+            cout << "Error at column 3: wrong symbols" << endl;
             IsDataTrue = false;
             break;
         }
     }
     if (lcount != 2 || rcount != 1) {
-        cout << "Error: wrong amount of data" << endl;
+        cout << "Error at column 4: wrong amount of data" << endl;
         IsDataTrue = false;
     }
     for (int i = right_par + 2; i < 80; i++) {
-        if (input[i] != ' ') {
-            cout << "Error: unexpected token" << endl;
+        if (circle[i] != ' ') {
+            cout << "Error at column 5: unexpected token" << endl;
             IsDataTrue = false;
             break;
         }
     }
     if (IsDataTrue) {
-        cout << inp;
+        cout << cir;
     }
     return 0;
 }
