a2ae11346ae20776254d56a4c61bd50da1a28623
unknown <you@example.com>
2021-03-01 13:40:18 +0700

    Delete ussless error

--- main.cpp
+++ main.cpp
@@ -22,37 +22,32 @@ int main()
                      * txt.compare(0, first_space, "CIRCLE")
                      * txt.compare(0, left_bracket, "CIRCLE");
     int c = txt.find('c');
-    int e = txt.find('e');
     while (1) {
         if (c - 1 == first_space) {
             cout << "Error at column 1: space before circle" << endl;
             Condition_is_met = false;
             break;
-        } else if (e + 1 == first_space) {
-            cout << "Error at column 2: space after circle" << endl;
-            Condition_is_met = false;
-            break;
         } else if (CircleTrue != 0) {
-            cout << "Error at column 3: circle not entered" << endl;
-            Condition_is_met = false;
+            cout << "Error at column 2: circle not entered" << endl;
+            Condition_is_met = false;	
             break;
         } else
             break;
     }
     int comma = txt.find(',');
     if (comma == -1) {
-        cout << "Error at column 4: comma not entered" << endl;
+        cout << "Error at column 3: comma not entered" << endl;
         Condition_is_met = false;
     }
     for (int i = left_bracket + 1; i <= comma; i++) {
         if ((txt[i] >= '0' && txt[i] <= '9') || txt[i] == '-' || txt[i] == ' ' || txt[i] == ',')
             continue;
         else if (txt[i] == '.') {
-            cout << "Error at column 5: coordinates must be integers" << endl;
+            cout << "Error at column 4: coordinates must be integers" << endl;
             Condition_is_met = false;
             break;
         } else {
-            cout << "Error at column 6: incorrect symbols before comma" << endl;
+            cout << "Error at column 5: incorrect symbols before comma" << endl;
             Condition_is_met = false;
             break;
         }
@@ -63,11 +58,11 @@ int main()
                 || txt[i] == ')')
                 continue;
             else if (txt[i] == '-') {
-                cout << "Error at column 7: radius cannot be negative" << endl;
+                cout << "Error at column 6: radius cannot be negative" << endl;
                 Condition_is_met = false;
                 break;
             } else {
-                cout << "Error at column 8: incorrect symbols after comma" << endl;
+                cout << "Error at column 7: incorrect symbols after comma" << endl;
                 Condition_is_met = false;
                 break;
             }
@@ -80,14 +75,14 @@ int main()
         if ((txt[k] >= '0' && txt[k] <= '9') || txt[k] == '-' || txt[k] == ',' || txt[k] == ' ')
             continue;
         else if (j > 3) {
-            cout << "Error at column 9: wrong amount of data" << endl;
+            cout << "Error at column 8: wrong amount of data" << endl;
             Condition_is_met = false;
             break;
         }
     }
     for (int i = right_bracket + 2; i < 80; i++) {
         if (text[i] != ' ') {
-            cout << "Error at column 10: extra characters" << endl;
+            cout << "Error at column 9: extra characters" << endl;
             Condition_is_met = false;
             break;
         }
