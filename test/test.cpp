fc5509c89bb06c9f75362337d5d18ad97af62980
unknown <you@example.com>
2021-05-27 05:42:36 +0700

    Fix test

--- test/test.cpp
+++ test/test.cpp
@@ -13,19 +13,12 @@ TEST_CASE("Circle")
 {
     SECTION("Errors")
     {
-        int left_bracket, right_bracket, first_space, comma, CircleTrue;
+        int left_bracket, right_bracket;
         string entered_data;
         char numbering[80] = "circle0 0, 1";
         entered_data.assign(numbering);
         left_bracket = entered_data.find('(');
         right_bracket = entered_data.find(')');
-        first_space = entered_data.find(' ');
-        CircleTrue = entered_data.compare(0, first_space, "circle")
-            * entered_data.compare(0, left_bracket, "circle")
-            * entered_data.compare(0, first_space, "CIRCLE")
-            * entered_data.compare(0, left_bracket, "CIRCLE");
-
-        comma = entered_data.find(',');
 
         strcpy(numbering, "circle0 0, 1)");
         CHECK(zero_fault(left_bracket, right_bracket) != 1);
