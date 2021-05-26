a2f3aa47160affc36a417e869a1ceb2f5185f5c0
unknown <you@example.com>
2021-05-27 02:25:23 +0700

    Using true clang-fornat

--- src/libgeometry/check.cpp
+++ src/libgeometry/check.cpp
@@ -4,113 +4,113 @@
 using namespace std;
 
 int zero_fault(int left_bracket, int right_bracket) {
-  if (left_bracket == -1 || right_bracket == -1)
-    return 0;
-  else
-    return 1;
+    if (left_bracket == -1 || right_bracket == -1)
+        return 0;
+    else
+        return 1;
 }
 
 int first_fault(int CircleTrue) {
-  if (CircleTrue != 0)
-    return 0;
-  else
-    return 1;
+    if (CircleTrue != 0)
+        return 0;
+    else
+        return 1;
 }
 
 int second_fault(int comma) {
-  if (comma == -1)
-    return 0;
-  else
-    return 1;
+    if (comma == -1)
+        return 0;
+    else
+        return 1;
 }
 
 int third_fault(int left_bracket, int comma, string entered_data) {
-  for (int i = left_bracket + 1; i < comma; i++) {
-    if (entered_data[i] == '.')
-      return 0;
-  }
-  return 1;
-}
-
-int fourth_fault(int left_bracket, int comma, string entered_data) {
-  for (int i = left_bracket + 1; i < comma; i++) {
-    if ((entered_data[i] >= '0' && entered_data[i] <= '9') ||
-        entered_data[i] == '-' || entered_data[i] == ' ')
-      continue;
-    else
-      return 0;
-  }
-  return 1;
-}
-
-int fifth_fault(int left_bracket, int comma, string entered_data) {
-  int count = 0;
-
-  for (int i = left_bracket + 1; i < comma; i++) {
-    if (entered_data[i] >= '0' && entered_data[i] <= '9') {
-      if (entered_data[i + 1] == ' ' || entered_data[i + 1] == ',')
-        count++;
+    for (int i = left_bracket + 1; i < comma; i++) {
+        if (entered_data[i] == '.')
+            return 0;
     }
-  }
-  if (count == 0 || count == 1)
-    return 0;
-  else
     return 1;
 }
 
-int sixth_fault(int comma, int right_bracket, string entered_data) {
-  for (int i = comma + 1; i < right_bracket; i++) {
-    if (entered_data[i] >= '0' && entered_data[i] <= '9') {
-      return 1;
+int fourth_fault(int left_bracket, int comma, string entered_data) {
+    for (int i = left_bracket + 1; i < comma; i++) {
+        if ((entered_data[i] >= '0' && entered_data[i] <= '9')
+            || entered_data[i] == '-' || entered_data[i] == ' ')
+            continue;
+        else
+            return 0;
     }
-  }
-  return 0;
+    return 1;
+}
+
+int fifth_fault(int left_bracket, int comma, string entered_data) {
+    int count = 0;
+
+    for (int i = left_bracket + 1; i < comma; i++) {
+        if (entered_data[i] >= '0' && entered_data[i] <= '9') {
+            if (entered_data[i + 1] == ' ' || entered_data[i + 1] == ',')
+                count++;
+        }
+    }
+    if (count == 0 || count == 1)
+        return 0;
+    else
+        return 1;
+}
+
+int sixth_fault(int comma, int right_bracket, string entered_data) {
+    for (int i = comma + 1; i < right_bracket; i++) {
+        if (entered_data[i] >= '0' && entered_data[i] <= '9') {
+            return 1;
+        }
+    }
+    return 0;
 }
 
 int seventh_fault(int comma, int right_bracket, string entered_data) {
-  for (int i = comma + 1; i < right_bracket; i++) {
-    if (entered_data[i] == '-')
-      return 0;
-  }
-  return 1;
+    for (int i = comma + 1; i < right_bracket; i++) {
+        if (entered_data[i] == '-')
+            return 0;
+    }
+    return 1;
 }
 
 int eighth_fault(int comma, int right_bracket, string entered_data) {
-  for (int i = comma + 1; i < right_bracket; i++) {
-    if ((entered_data[i] >= '0' && entered_data[i] <= '9') ||
-        entered_data[i] == '.' || entered_data[i] == ' ')
-      continue;
-    else
-      return 0;
-  }
-  return 1;
+    for (int i = comma + 1; i < right_bracket; i++) {
+        if ((entered_data[i] >= '0' && entered_data[i] <= '9')
+            || entered_data[i] == '.' || entered_data[i] == ' ')
+            continue;
+        else
+            return 0;
+    }
+    return 1;
 }
 
 int ninth_fault(int left_bracket, int right_bracket, string entered_data) {
-  int count = 0;
+    int count = 0;
 
-  for (int i = left_bracket + 1; i < right_bracket; i++) {
-    if (count > 1)
-      return 0;
-    if (entered_data[i] == ',' || entered_data[i] == ')' ||
-        entered_data[i] == '(')
-      count++;
-    if ((entered_data[i] >= '0' && entered_data[i] <= '9') ||
-        entered_data[i] == '-' || entered_data[i] == ',' ||
-        entered_data[i] == ' ' || entered_data[i] == '.')
-      continue;
-    else
-      return 0;
-  }
-  return 1;
+    for (int i = left_bracket + 1; i < right_bracket; i++) {
+        if (count > 1)
+            return 0;
+        if (entered_data[i] == ',' || entered_data[i] == ')'
+            || entered_data[i] == '(')
+            count++;
+        if ((entered_data[i] >= '0' && entered_data[i] <= '9')
+            || entered_data[i] == '-' || entered_data[i] == ','
+            || entered_data[i] == ' ' || entered_data[i] == '.')
+            continue;
+        else
+            return 0;
+    }
+    return 1;
 }
 
 int tenth_fault(int right_bracket, char numbering[]) {
-  for (int i = right_bracket + 2; i < 80; i++) {
-    if (numbering[i] == ' ')
-      continue;
-    else
-      return 0;
-  }
-  return 1;
+    for (int i = right_bracket + 2; i < 80; i++) {
+        if (numbering[i] == ' ')
+            continue;
+        else
+            return 0;
+    }
+    return 1;
 }
--- src/libgeometry/computation.cpp
+++ src/libgeometry/computation.cpp
@@ -5,42 +5,44 @@
 using namespace std;
 
 double computation_area(int comma, int right_bracket, string entered_data) {
-  int count = 0;
-  double area = 0, PI = 3.141592653589793;
-  char radius[80];
+    int count = 0;
+    double area = 0, PI = 3.141592653589793;
+    char radius[80];
 
-  for (int i = comma + 1; i < right_bracket; i++) {
-    if (entered_data[i] == '-' || entered_data[i] == ' ')
-      continue;
-    else if ((entered_data[i] >= '0' && entered_data[i] <= '9') ||
-             entered_data[i] == '.') {
-      radius[count] = entered_data[i];
-      count++;
-    } else {
-      break;
+    for (int i = comma + 1; i < right_bracket; i++) {
+        if (entered_data[i] == '-' || entered_data[i] == ' ')
+            continue;
+        else if (
+                (entered_data[i] >= '0' && entered_data[i] <= '9')
+                || entered_data[i] == '.') {
+            radius[count] = entered_data[i];
+            count++;
+        } else {
+            break;
+        }
     }
-  }
 
-  return area = PI * atof(radius) * atof(radius);
+    return area = PI * atof(radius) * atof(radius);
 }
 
-double computation_perimeter(int comma, int right_bracket,
-                             string entered_data) {
-  int count = 0;
-  double perimeter = 0, PI = 3.141592653589793;
-  char radius[80];
+double
+computation_perimeter(int comma, int right_bracket, string entered_data) {
+    int count = 0;
+    double perimeter = 0, PI = 3.141592653589793;
+    char radius[80];
 
-  for (int i = comma + 1; i < right_bracket; i++) {
-    if (entered_data[i] == '-' || entered_data[i] == ' ')
-      continue;
-    else if ((entered_data[i] >= '0' && entered_data[i] <= '9') ||
-             entered_data[i] == '.') {
-      radius[count] = entered_data[i];
-      count++;
-    } else {
-      break;
+    for (int i = comma + 1; i < right_bracket; i++) {
+        if (entered_data[i] == '-' || entered_data[i] == ' ')
+            continue;
+        else if (
+                (entered_data[i] >= '0' && entered_data[i] <= '9')
+                || entered_data[i] == '.') {
+            radius[count] = entered_data[i];
+            count++;
+        } else {
+            break;
+        }
     }
-  }
 
-  return perimeter = 2 * PI * atof(radius);
+    return perimeter = 2 * PI * atof(radius);
 }
