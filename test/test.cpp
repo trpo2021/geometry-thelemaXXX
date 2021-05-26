3b47d7dc56445c029ed06c1d98ad9351311129d6
unknown <you@example.com>
2021-05-27 05:33:41 +0700

    Add test

---
+++ test/test.cpp
@@ -0,0 +1,38 @@
+#define CATCH_CONFIG_MAIN
+#include "../thirdparty/catch.hpp"
+#include "libgeometry/check.h"
+#include "libgeometry/computation.h"
+#include "libgeometry/crossing.h"
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <cmath>
+using namespace std;
+
+TEST_CASE("Circle")
+{
+    SECTION("Errors")
+    {
+        int left_bracket, right_bracket, first_space, comma, CircleTrue,
+            Condition_is_met = 1;
+        char numbering[80];
+        string entered_data;
+
+        for (int i = 0; i < 80; i++)
+            numbering[i] = ' ';
+        numbering[80] = "circle0 0, 1";
+        entered_data.assign(numbering);
+        left_bracket = entered_data.find('(');
+        right_bracket = entered_data.find(')');
+        first_space = entered_data.find(' ');
+        CircleTrue = entered_data.compare(0, first_space, "circle")
+            * entered_data.compare(0, left_bracket, "circle")
+            * entered_data.compare(0, first_space, "CIRCLE")
+            * entered_data.compare(0, left_bracket, "CIRCLE");
+
+        comma = entered_data.find(',');
+        
+        strcpy(numbering, "circle0 0, 1)");
+        CHECK(zero_fault(left_bracket, right_bracket) != 1);
+    }
+}
